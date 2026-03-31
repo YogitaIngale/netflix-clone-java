841706ad2d1f1e2a43fc83a1bc2434377b5078a6
Yogita Ingale <ingyogita95@gmail.com>
2026-03-31 10:48:54 +0530

    Update UploadLog.java

--- NetflixClone/src/main/java/com/yogita/netflix/model/UploadLog.java
+++ NetflixClone/src/main/java/com/yogita/netflix/model/UploadLog.java
@@ -28,7 +28,7 @@ public class UploadLog {
         this.uploadDate = LocalDateTime.now();
     }
 
-    // getters & setters
+    
     public Integer getId() { return id; }
     public void setId(Integer id) { this.id = id; }
 
